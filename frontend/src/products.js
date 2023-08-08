80e78b77eaccb6b025b98aa33ef54cadd72a8e37
ricardomb94 <aymardmb@gmail.com>
2023-08-09 00:17:14 +0200

    React image carousel library  added

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -3,7 +3,7 @@ const products = [
     _id: 1,
     name: 'Audi A1',
     image: '/images/audi-slider1.jpeg',
-    thumbnail:[],
+    thumbnails:["https://picsum.photos/id/1018/250/150/",],
     description:
       'L\'Audi A1 est une voiture à hayon élégante et compacte qui respire la sophistication et la modernité. Elle présente un design extérieur élégant caractérisé par des lignes nettes, une calandre avant agressive et des phares à LED. Sa taille compacte en fait une voiture idéale pour la ville, permettant une maniabilité et un stationnement faciles.',
     brand: 'Audi A1',
@@ -18,6 +18,7 @@ const products = [
     _id: 2,
     name: 'Volgswagen Up',
     image:'/images/volgswagen-slider5.jpeg',
+    thumbnails:["https://picsum.photos/id/1018/250/150/",],
     description:
       'Le Volswagen Up! est une petite voiture citadine compacte connue pour sa taille réduite, sa praticité et son efficacité. Elle arbore un design moderne, un intérieur étonnamment spacieux pour sa catégorie et diverses fonctionnalités et technologies modernes. Avec sa maniabilité réactive et ses options respectueuses de l\'environnement, elle est bien adaptée à la conduite en ville et aux déplacements quotidiens.',
     brand: 'Volswagen',
@@ -32,7 +33,7 @@ const products = [
     _id: 3,
     name: 'Nissan Pixo',
     image: '/images/nissan-slider1.jpeg',
-      
+    thumbnails:["https://picsum.photos/id/1018/250/150/",],  
     description:
       'Le Nissan Pixo est une petite voiture citadine connue pour sa taille compacte, son efficacité énergétique et son prix abordable. ',
     brand: 'Sony',
@@ -47,6 +48,7 @@ const products = [
     _id: 2,
     name: 'Volgswagen Up',
     image:'/images/volgswagen-slider5.jpeg',
+    thumbnails:["https://picsum.photos/id/1018/250/150/",],
     description:
       'Le Volswagen Up! est une petite voiture citadine compacte connue pour sa taille réduite, sa praticité et son efficacité. Elle arbore un design moderne, un intérieur étonnamment spacieux pour sa catégorie et diverses fonctionnalités et technologies modernes. Avec sa maniabilité réactive et ses options respectueuses de l\'environnement, elle est bien adaptée à la conduite en ville et aux déplacements quotidiens.',
     brand: 'Volswagen',
@@ -61,7 +63,7 @@ const products = [
     _id: 3,
     name: 'Nissan Pixo',
     image: '/images/nissan-slider4.jpeg',
-      
+    thumbnails:["https://picsum.photos/id/1018/250/150/",],
     description:
       'Le Nissan Pixo est une petite voiture citadine connue pour sa taille compacte, son efficacité énergétique et son prix abordable. ',
     brand: 'Sony',
@@ -76,6 +78,7 @@ const products = [
     _id: 1,
     name: 'Audi A1',
     image: '/images/audi-slider1.jpeg',
+    thumbnails:["https://picsum.photos/id/1018/250/150/",],
     description:
       'L\'Audi A1 est une voiture à hayon élégante et compacte qui respire la sophistication et la modernité. Elle présente un design extérieur élégant caractérisé par des lignes nettes, une calandre avant agressive et des phares à LED. Sa taille compacte en fait une voiture idéale pour la ville, permettant une maniabilité et un stationnement faciles.',
     brand: 'Audi A1',
