ddab8e2582eaa44c701816c4b0b9fb1eef15c251
ricardomb94 <aymardmb@gmail.com>
2023-08-10 17:00:58 +0200

    Clean up server.jd

--- backend/server.js
+++ backend/server.js
@@ -27,9 +27,9 @@ app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/upload', uploadRoutes)
 
-app.get('/api/config/paypal', (req, res) =>
-  res.send( process.env.PAYPAL_CLIENT )
-)
+// app.get('/api/config/paypal', (req, res) =>
+//   res.send( process.env.PAYPAL_CLIENT )
+// )
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
