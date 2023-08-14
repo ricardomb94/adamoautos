52404410b6c2dd3fe3a30be50c73e91a31e9e2b7
ricardomb94 <aymardmb@gmail.com>
2023-08-14 18:18:44 +0200

    Remodeling products objet & ProductDetails

--- frontend/src/components/ProductDetails.js/index.js
+++ frontend/src/components/ProductDetails.js/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import products from '../../products';
-import { Card } from 'react-bootstrap';
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 import SingleSidebar from '../SingleSidebar';
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css"
+import sd3 from '../../images/project/img-10.jpg'
 import './style.css'
 
 const ProductDetails = () => {
@@ -24,8 +25,11 @@ const ProductDetails = () => {
             <div className="hx-service-dt-area hx-section-padding">
             <div className="container">
                 <div className="row">
-                    <SingleSidebar/>
                     <div className="col-lg-8">
+                    <div className="prv-nx">
+                            <div className="pre-btn"><h2>{product.brand}</h2></div>
+                            {/* <div className="nex-btn"><Link to="/service-details">Next</Link></div> */}
+                        </div>
                         <div className="hx-service-dt-right">
                             {/* <div className="hx-service-dt-img">
                                 <img src={product.image} alt=""/>
@@ -34,24 +38,65 @@ const ProductDetails = () => {
                                 <Card className="my-3 p-3 rounded">
                                     <ImageGallery 
                                         items={images} 
-                                        alt={product.name}
+                                        alt={product.brand}
                                         thumbnailPosition="left"
                                         />
 
                                 </Card>
                            
                             </div>
-                            <p><span></span>{product.description}</p>
-                            <blockquote>Veuillez nous contacter pour plus de détails !</blockquote>
+                            <h3 className='title-list'>Caractéristiques</h3>
+                            
+                            <ListGroup>
+                                <ListGroupItem>
+                                    Année: {product.year}
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                    Provenance: {product.provenance}
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                     Mise en circulation: {product.registration}    
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                    Contrôle technique: {product.vehicleInspection}
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                    Première main: {product.originalOwner}
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                    Kilométrage compteur: {product.odometerReading}
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                    Energie: {product.energy}
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                    Boite vitesse: {product.transmission}
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                    Sellerie: {product.upholstery}
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                    Nombre de portes: {product.doors}
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                    Nombre de places: {product.seats}
+                                </ListGroupItem>
+                                <ListGroupItem>
+                                    Nombre de places: {product.color}
+                                </ListGroupItem>
+                          </ListGroup>
+                              
+                            {/* <blockquote>Veuillez nous contacter pour plus de détails !</blockquote>
                             <div className="hx-service-dt-s">
-                                <img src={product.image} alt="" />
-                            </div>
+                                <img src={sd3} alt="" className="img img-responsive"/>
+                            </div> */}
                         </div>
-                        <div className="prv-nx">
+                        <div className="prv-nx mt-3">
                             <div className="pre-btn"><Link to="/catalogue">Retour au Catalogue</Link></div>
                             {/* <div className="nex-btn"><Link to="/service-details">Next</Link></div> */}
                         </div>
                     </div>
+                    <SingleSidebar className="mt-3"/>
                 </div>
             </div>
         </div>
--- frontend/src/components/Services/index.js
+++ frontend/src/components/Services/index.js
@@ -5,57 +5,64 @@ import './style.css'
 
 const Services = (props) => {
 
+    console.log('***Services props***', props);
+
     const service = [
         {
+            id:1,
             icon: 'fi flaticon-car-repair',
             title: 'Diagnostique et entretien',
             des:'Vidange + filtre à huile, Révision complète, Freinage: Changement de plaquettes de frein, Liquide de frein, Frein à tambour',
             link:'/service'
         },
         {
+            id:2,
             icon: 'fi flaticon-battery',
             title: 'Charge et Démarrage',
             des:'Contrôle de la batterie, Réparation du système de charge, et démarrage. Remplacement de la batterie si nécessaire',
             link:'/service'
         },
         {
+            id:3,
             icon: 'fi flaticon-electricity',
             title: 'Embrayage',
             des: "Diagnostic et ajustement, Remplacement du kit d'embrayage, Contrôle de l'embrayage et de la boîte de vitesses.",
             link:'/service'
         },
         {
+            id:4,
             icon: 'fi flaticon-turbo',
             title: 'Moteur',
             des:"Diagnostic et réparation, Révision complète du moteur, Remplacement des pièces défectueuses, Contrôle des fluides et de la performance moteur, Entretien pour une efficacité optimale.",
             link:'/service'
         },
         {
+            id:5,
             icon: 'fi flaticon-tyre',
             title: 'Roues',
             des:"Équilibrage et alignement, Changement de pneus, Contrôle de la pression des pneus, Rotation des pneus pour une usure uniforme, Entretien des roues pour une conduite sûre et confortable.",
             link:'/service'
         },
         {
-            icon: 'fi flaticon-car-1',
+            id:6,            icon: 'fi flaticon-car-1',
             title: 'Direction et Transmission',
             des:"Diagnostic et ajustement, Entretien de la direction assistée, Réparation ou remplacement des composants, Contrôle du système de transmission pour une performance fluide.",
             link:'/service'
         },
         {
-            icon: 'fi flaticon-car-2',
+            id:7,            icon: 'fi flaticon-car-2',
             title: 'Suspension',
             des:"Vérification et réglage, Remplacement des amortisseurs ou des ressorts, Contrôle de la suspension pour une conduite stable, Entretien pour une absorption optimale des chocs",
             link:'/service'
         },
         {
-            icon: 'fi flaticon-car-2',
+            id:8,            icon: 'fi flaticon-car-2',
             title: 'Echappement',
             des:"Inspection et réparation, Remplacement des composants défectueux, Contrôle des émissions, Entretien du système d'échappement dans le respect des normes environnementales",
             link:'/service'
         },
         {
-            icon: 'fi flaticon-car-2',
+            id:9,            icon: 'fi flaticon-car-2',
             title: 'Distribution',
             des:"Vérification du système de distribution, Changement de la courroie de distribution, Contrôle des composants, Maintenance pour la fiabilité et la synchronisation du moteur",
             link:'/service'
