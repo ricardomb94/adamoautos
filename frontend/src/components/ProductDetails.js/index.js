9d15e037ab1382801b2334f89276e2666f272a8f
ricardomb94 <aymardmb@gmail.com>
2023-09-10 10:49:29 +0200

    SingleSideBar modified

--- frontend/src/components/ProductDetails.js/index.js
+++ frontend/src/components/ProductDetails.js/index.js
@@ -89,13 +89,14 @@ const ProductDetails = ({props}) => {
                                 </ListGroupItem>
                           </ListGroup>
                         </div>
+                        <SingleSidebar className="mt-3"/>
                         <div className="prv-nx mt-3">
                             <div className="pre-btn"><Link to="/catalogue">Retour au Catalogue</Link></div>
                             {/* <div className="nex-btn"><Link to="/service-details">Next</Link></div> */}
                         </div>
                     </div>
                 </div>
-                <SingleSidebar className="mt-3"/>
+                
             </div>
         </div>
         );
